eb3f3c5013636f3b05a06705f62b595840b5ae8e
Pankaj Pathak <matwaripankaj@gmail.com>
2024-02-16 14:56:04 +0530

    Updating API details

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = "1d3a0eefa97b499d8fbc4ee93eeb40b7";
+const API_KEY = "c38a299ef5994eaeacf223c231631f80";
 const url = "https://newsapi.org/v2/everything?q=";
 
 window.addEventListener("load", () => fetchNews("India"));
